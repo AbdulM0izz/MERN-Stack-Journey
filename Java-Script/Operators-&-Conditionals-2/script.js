bcb067aaaf3a180644ce0782d21c78189f657ad9
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-02-19 13:21:32 +0500

    Operatoers & Conditionals Done

---
+++ Java-Script/Operators-&-Conditionals-2/script.js
@@ -0,0 +1,34 @@
+// arithmetic operators
+
+// let a = 10;
+// let b = 5;
+
+// console.log(a * b);
+
+// a += 10;
+// a = a + 10;
+// console.log(a);
+
+// uniary operatoers
+
+// strict and loose equality
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a === b); // false
+
+// console.log('5' == 5); //true
+// console.log('5' === 5); //false
+
+// console.log(5 != 6);
+
+// ternary operator
+
+let age = 25;
+let status = (age > 18) ? "you can vote" : "you cannot";
+console.log(status);
+
+// let msg = "y";
+// let sts = (msg == "y") ? "you can" : "you canot";
+// console.log(sts);
