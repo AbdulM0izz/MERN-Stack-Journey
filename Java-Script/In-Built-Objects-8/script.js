4cc249f34cd33c2dcd21f34e4a572f9964984ac5
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-02-26 13:43:25 +0500

    Built-in objects Done

---
+++ Java-Script/In-Built-Objects-8/script.js
@@ -0,0 +1,32 @@
+// use of math objects
+
+console.log(Math.PI); //value of pi
+
+console.log(Math.max(29, 90, 30, 30)); //max num
+
+console.log(Math.min(29, 10, 89, 90)); //min num
+
+console.log(Math.round(6.7)); //round off
+console.log(Math.floor(1.9)); //just smaller int
+
+console.log(Math.ceil(1.1)); //just bigger int
+
+console.log(Math.abs(-1)); //neg to pos and pos to pos
+
+console.log(Math.random()); //num zero to  1 random
+
+console.log(Math.sqrt(4)); //square root
+
+console.log(Math.pow(2, 10)); //2^10
+
+// Date and time object
+
+let curr = new Date();
+
+console.log(curr);
+
+let newdate = new Date(1998, 5, 20, 7);
+
+console.log(newdate.setFullYear(2001));
+
+console.log(newdate);
