0f9caae9bd603dff1c1eeb4ab9c6ad50d687a979
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-03-01 14:13:11 +0500

    Dom Manipulation Done

---
+++ Java-Script/Dom-Manipulation-12/script.js
@@ -0,0 +1,28 @@
+//access the html tag 
+
+//html file is document
+
+// document.getElementById("fpara");
+
+//access by class 
+// document.getElementsByClassName("ch");
+
+//access by tag name
+// document.getElementsByTagName("h2");
+
+// method to add new elemeny by insertAdjacentElement;
+
+let mydiv = document.querySelector('#mydiv');
+
+let newelement = document.createElement('span');
+
+newelement.textContent = "new element";
+mydiv.insertAdjacentElement('afterend', newelement);
+
+// remove element
+// first need parent
+
+let parent = document.querySelector('#mydiv');
+let child = document.querySelector('#fpara');
+
+parent.removeChild(child);
