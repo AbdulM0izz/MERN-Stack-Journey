2fab3ee2ab336433a03c9510cd4c40902ab46590
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-03-02 15:37:08 +0500

    Style elements with js

---
+++ Java-Script/Styling-Elements-13/script.js
@@ -0,0 +1,7 @@
+let secondelement = document.getElementById('sdiv');
+
+secondelement.cssText;
+
+secondelement.style.cssText = " background-color: blue; color: white; padding: 5rem;"
+
+let firstele = document.querySelector('#fdiv')
