f6ab20f0199541c4da8f69b7558cebc03dfa610b
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-03-07 18:02:53 +0500

    Use of await and fetch-api

---
+++ Java-Script/Async-Await-Fetch-Api-17/script.js
@@ -0,0 +1,27 @@
+// syntax of async function
+
+// async function getdata() {
+
+//     setTimeout(function() {
+//         console.log("in time out block");
+//     }, 3000);
+
+// }
+// getdata();
+
+
+// fetch api ( get request using fetch api)
+
+
+async function getdata() {
+    //get request == asyn operation
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+
+
+    // data parsing or converting in json form => is also asyn operation  we use await
+    let data = await response.json();
+
+    console.log(data);
+}
+
+getdata();
