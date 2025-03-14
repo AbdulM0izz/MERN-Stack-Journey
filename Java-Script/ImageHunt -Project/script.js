f375b0a36614fa09e56e446e04a91d5236edbc28
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-03-14 16:20:20 +0500

    Image-Hunt api project done

--- Java-Script/ImageHunt -Project/script.js
+++ Java-Script/ImageHunt -Project/script.js
@@ -1,24 +1,29 @@
 const accessKey = '9hSpuTzbS234HVuwd3rdPWuJnvMo7RoOSPmG-eNs3Oc';
-const searchinput = document.querySelector('.search-input');
-const imagesContainer = document.querySelector('.images-conatiner');
+const searchInput = document.querySelector('.search-input');
+const imagesContainer = document.querySelector('.images-container');
 const searchForm = document.querySelector('form');
 
-
-
 const fetchImages = async(query) => {
-
-    const url = 'https://api.unsplash.com/photos/?query=&{query}&client_id=DxWKJzOE9ny3fvLko1a-xgTLT_atcTIBT5srg9pAQSw&per_page=28';
+    const url = `https://api.unsplash.com/search/photos?query=${query}&client_id=${accessKey}&per_page=28`;
 
     const response = await fetch(url);
-    const data = response.json();
+    const data = await response.json();
     console.log(data);
+
+    data.results.forEach(photo => {
+        const imageElement = document.createElement('div');
+        imageElement.innerHTML = `<img src="${photo.urls.regular}" alt="Unsplash Image"/>`;
+        imagesContainer.appendChild(imageElement);
+    });
 }
+
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault(); // Stop auto submit
-    const inputtext = searchinput.value.trim();
+    const inputText = searchInput.value.trim();
 
-    if (inputtext !== '') {
-        fetchImages(inputtext);
+    if (inputText !== '') {
+        imagesContainer.innerHTML = ''; // Clear previous results
+        fetchImages(inputText);
     } else {
         imagesContainer.innerHTML = '<h3>Enter query to search</h3>';
     }
