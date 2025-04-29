d11f4d1d8065e30061b62b9a05c0117e99aaf999
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-04-30 01:12:49 +0500

    Delete api

---
+++ React/DeleteApi-46/api/src/App.jsx
@@ -0,0 +1,13 @@
+import Deleteapi from './DeleteApi'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     < Deleteapi />
+    </>
+    
+  )
+}
+
+export default App
---
+++ React/DeleteApi-46/api/src/DeleteApi.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+//for delete first get data 
+
+const DeleteApi = () => {
+  return (
+    <>
+        <h2>DeleteApi</h2>
+    </>
+  )
+}
+
+export default DeleteApi
