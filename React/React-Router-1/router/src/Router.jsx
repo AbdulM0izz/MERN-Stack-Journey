1de6d9ea0de3960128effcd8a487f63f6186a74d
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-05-06 16:33:37 +0500

    React Router V6

---
+++ React/React-Router-1/router/src/Router.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Router = () => {
+  return (
+    <div>
+      <h1>Intro</h1>
+    </div>
+  )
+}
+
+export default Router
+/***
+ * When we have to show mulitple page web we use react router v6
+ * if we use ancer tag our web page will reload 
+ * If we use react router our page and content will change but page will not refresh
+ * 
+ */
