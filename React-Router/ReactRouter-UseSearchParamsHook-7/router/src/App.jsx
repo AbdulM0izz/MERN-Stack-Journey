f3f1addc2f2600d4ecc3a82a1e4469fde930a41c
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-05-10 01:16:17 +0500

    React router usesearchparams hook

---
+++ React-Router/ReactRouter-UseSearchParamsHook-7/router/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import {BrowserRouter, Route, Routes,Navigate} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Nav from './Nav'
+import Search from './Search'
+function App() {
+  return (
+    <>
+    
+    <BrowserRouter>
+      <Nav />
+    <Routes>
+      <Route path='/home' element={<Home />} />
+      <Route path='/About' element={<About />}/>
+      <Route path='*' element={<Navigate to="/home" />}/>
+      <Route path='/search' element={<Search />}/>
+    </Routes>
+    
+    </BrowserRouter>
+   
+    </>
+    
+  )
+}
+
+export default App
---
+++ React-Router/ReactRouter-UseSearchParamsHook-7/router/src/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    /**
+     * style={({isactive})=> {return {backgroundColor: isactive ? 'aqua' : 'yellow'}}} 
+     */
+    <div>
+        <ul className='nav'> 
+            <li><NavLink className='nav-link' to="/home" >Home</NavLink></li>
+            <li><NavLink className='nav-link'to="/About">About</NavLink></li>
+            <li><NavLink className='nav-link'to="/search">Search</NavLink></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
