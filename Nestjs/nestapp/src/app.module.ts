eca2b9e323f9b4c481c566f90dabb9fd5ebc7095
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-05-31 02:36:27 +0500

    RestApi Creation

--- Nestjs/nestapp/src/app.module.ts
+++ Nestjs/nestapp/src/app.module.ts
@@ -6,9 +6,10 @@ import { ProductService } from './product/product.service';
 import { ProductController } from './product/product.controller';
 import { EmployeModule } from './employe/employe.module';
 import { CategoryModule } from './category/category.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [EmployeModule, CategoryModule],
+  imports: [EmployeModule, CategoryModule, StudentModule],
   controllers: [AppController, UserController, ProductController],
   providers: [AppService, ProductService],
 })
---
+++ Nestjs/nestapp/src/student/student.service.ts
@@ -0,0 +1,39 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
+
+@Injectable()
+export class StudentService {
+    private studentsData = [
+        {id: 1, name: 'Moiz', age:19},
+        {id:2, name: 'Ali', age:20}
+    ];
+     //Get 
+    getAllStudents () {
+        return this.studentsData;
+    }
+    getStudentById (id: number) {
+        const student = this.studentsData.find((s)=> s.id === id);
+        if (!student) throw new NotFoundException('Data Not found');
+        return student;
+    }
+    //Post
+    createStudent (data: {name: string, age: number}){
+        const newStudents = {
+            id: Date.now(),
+            ...data
+        };
+        this.studentsData.push(newStudents);
+        return newStudents;
+    }
+    //Put
+    updateData (id: number, data: {name: string, age: number}) {
+        const index = this.studentsData.findIndex((s)=> s.id === id);
+        if (index === -1) throw new NotFoundException('Not found');
+        this.studentsData[index] = {id, ...data};
+        return this.studentsData[index];
+
+    }
+    //Patch
+    
+}
