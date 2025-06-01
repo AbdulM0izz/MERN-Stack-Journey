88ad552a87a5380c27417ae68ab61bb5055e5ffc
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-06-02 02:20:00 +0500

    Dto class validator

--- Nestjs/nestapp/src/costumer/dto/create-costumer.dto.ts
+++ Nestjs/nestapp/src/costumer/dto/create-costumer.dto.ts
@@ -1,6 +1,10 @@
 /* eslint-disable prettier/prettier */
 
+import { IsNumber, IsString } from "class-validator";
+
 export class createCostumerDto {
+    @IsString()
     name: string;
+    @IsNumber()
     age: number;
 }
